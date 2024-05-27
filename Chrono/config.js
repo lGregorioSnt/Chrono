b6fb289cee099a6ebc5acc9a9055aff67a3921bc
lGregorioSnt <lucas.gregorio.lgs@gmail.com>
2024-05-27 16:47:44 +0000

    est

---
+++ Chrono/config.js
@@ -0,0 +1,4 @@
+document.getElementById('tema-button').addEventListener('click', function() {
+    // Adiciona a classe 'dark-theme' ao body
+    document.body.classList.toggle('dark-theme');
+  });
